439549981a69a9b4c58d8f14a1dee7d8be48237c
Joseph Dykstra <josephdykstra@gmail.com>
2014-04-09 21:49:37 -0500

    2014-04-09 21:49 - Updated comments on scriptHang()

--- Autos/7-Scripts.c
+++ Autos/7-Scripts.c
@@ -2,8 +2,8 @@ void scriptHang()
 	{
 	autoResetStart(0, SCRIPT, true, true, false);
 		//L,	R,		S,		Lift,	Intk,Cata,Tranny,End Type,	Other
-	//auto(straight(HALF),0,	lPre(STS),	0,	0,	0,	TIME_LIMIT,	100);
-	//auto(straight(REV),	0,	lPre(GND),	0,	0,	1,	TIME_LIMIT,	5000);
-	auto(straight(REV),	0,		0,			0,	0,	1,	TIME_LIMIT,	5000);
+	//auto(straight(HALF),0,	lPre(STS),	0,	0,	0,	TIME_LIMIT,	100);	//Drive into bar
+	//auto(straight(REV),	0,	lPre(GND),	0,	0,	1,	TIME_LIMIT,	5000);	//Hang
+	auto(straight(REV),	0,		0,			0,	0,	1,	TIME_LIMIT,	5000);	//Hang
 	autoResetEnd();
 	}
